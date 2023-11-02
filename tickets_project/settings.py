12fb3d2968aefb71d55e9e7b820c6ed51156ab4b
alesanfe <alex.0002002@gmail.com>
2023-11-02 13:43:40 +0100

    Mejorado viasualmente

--- tickets_project/settings.py
+++ tickets_project/settings.py
@@ -90,15 +90,6 @@ DATABASES = {
         conn_max_age=600
     )
 }
-'''
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-'''
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
